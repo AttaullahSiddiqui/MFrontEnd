a1de8c5da48325572aca6f3e73e892851a601b05
server <dev@mmcgbl.com>
2020-04-10 22:27:17 +0500

    Working on Dashboard page

--- src/app/featured-module/private/private-routing.module.ts
+++ src/app/featured-module/private/private-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [{
-  path: '',
+  path: 'dashboard',
   loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
 },{
   path: 'payment',
