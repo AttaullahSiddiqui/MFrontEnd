258441188db6b83f1a99848ad7897c31d752f506
zehrazaidi96 <syedaz3hrazaidi1996@gmail.com>
2020-04-14 19:46:12 +0500

    changes in login html,ts and app module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,21 @@ import { HeaderComponent } from './core/components/header/header.component';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { SideMenuComponent } from './core/components/side-menu/side-menu.component';
 import { LayoutComponent } from './core/components/layout/layout.component';
+import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider, GoogleLoginProvider } from 'angularx-social-login';
+
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("1066138280146-44i44jb6aud2e88urvtpkc4cc06kolmj.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('225062682140416')
+  }
+]);
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -20,9 +35,15 @@ import { LayoutComponent } from './core/components/layout/layout.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
