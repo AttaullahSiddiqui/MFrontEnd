8af090559ce9fce86cc9abb38285c126298dc842
Muhammad Mateen <mateen@mmcgbl.com>
2020-05-12 13:32:56 +0500

    update root url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://abc.mmcgbl.com/api/v1/'
+  baseUrl: 'https://abc.mmcgbl.com/api/v1/',
+  rootUrl: 'https://abc.mmcgbl.com/'
 };
