e99726860c14117115e53d187452b05b59af934e
server <dev@mmcgbl.com>
2020-05-13 13:42:53 +0500

    working on plan (btn) and header

--- src/app/featured-module/public/home/header/header.component.ts
+++ src/app/featured-module/public/home/header/header.component.ts
@@ -10,26 +10,9 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
-
-  
   ngOnInit(): void {
-    
-    // Open links in mobiles
-function handleSmallScreens() {
-  document.querySelector('.navbar-toggler')
-    .addEventListener('click', () => {
-    let navbarMenu = document.querySelector('.navbar-menu')
-
-    // if (navbarMenu.style.display === 'flex') {
-    //   navbarMenu.style.display = 'none'
-    //   return
-    // }
-
-    // navbarMenu.style.display = 'flex'
-  })
-}
-
-handleSmallScreens()
+  
   }
 
 }
+
