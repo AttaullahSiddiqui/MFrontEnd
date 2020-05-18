cfffa5bcc318d96f771a19604e134c3076dbb2b8
server <dev@mmcgbl.com>
2020-05-18 05:50:10 +0500

    woring on sidenav and check and fixed responsiveness

--- src/app/featured-module/public/not-found404/not-found404.component.ts
+++ src/app/featured-module/public/not-found404/not-found404.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-not-found404',
   templateUrl: './not-found404.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFound404Component implements OnInit {
 
-  constructor() { }
+  constructor() { 
+ 
+    
+  }
 
   ngOnInit(): void {
+    $('#menu li a').on('click', function(){
+    $('li a.current').removeClass('current');
+    $(this).addClass('current');
+});
+  
   }
 
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   baseUrl: 'http://localhost:5000/api/v1/',
   rootUrl: 'http://localhost:5000/'
+
 };
 
 /*
