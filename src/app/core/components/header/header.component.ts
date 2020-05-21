ebe45f38bbf8941548d4fa593399b498ba3a67ac
muhammadmateen <muhammadmateen02@gmail.com>
2020-05-21 11:55:44 +0500

    fix circular dependency issue

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UtilityService } from '@app/core';
+import { UtilityService } from '../../services/utility.service';
 import { Router } from '@angular/router'
 
 @Component({
